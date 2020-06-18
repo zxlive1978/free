48f832cf099ab54c6bce78ac1254f1000082b7ea
zxlive1978 <zxlive@yandex.ru>
2020-06-18 10:30:59 +0400

    jquery modal

--- index.php
+++ index.php
@@ -10,8 +10,11 @@
 
 <!-- Bootstrap -->
 <link href="css/bootstrap.min.css" rel="stylesheet">
-
+<!-- Jquery -->
+<link rel="stylesheet" href="css/jquery-ui.min.css">
+<link rel="stylesheet" href="css/style.css">
 <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="js/jquery-ui.min.js"></script>
 <!-- Bootstrap --> 
 <script src="js/popper.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
