0bd20118560bbd75283e8689930a7e7baaed3fd1
zxlive1978 <zxlive@yandex.ru>
2020-10-02 16:15:51 +0400

    3123

--- js/writeskvs.js
+++ js/writeskvs.js
@@ -59,3 +59,46 @@ function writeskvs(whatdo, table, p000, skvsjson){
 	});
 
 };
+
+//Запись скважины
+function readskvs(whatdo, table, p000, skvsjson){
+	refresh = false;
+	online = false;
+ 
+	var data = null;
+	$.ajax({
+		type: "POST",
+		url: 'js/writeskvs.php',
+		data: {whatdo:whatdo, table: table, p000: p000, skvsjson:skvsjson },
+		cache: false,
+		async: false,
+		success: function(data){
+			//if (Number(data)!=1) {alert('нет связи')};
+			//console.log(data);
+			var mass = eval(data);
+			//var mass = data;
+			console.log(data);
+			//alert(mass);
+			//d110d = null;
+			//d110d = mass.slice();
+			
+			//numbs110d = null;
+			
+			
+			//А ЭТО АХУЕННО ИНТЕРЕСНО!
+			//Если нет данных
+			/* if (d110d.length == 0){
+				drawGraf =false ;
+				d110d = beg_rec.slice();
+				d110d[0]["Vrema"] = start_time;
+				d110d[d110d.length-1]["Vrema"] = end_time;
+				//console.log (beg_rec);
+				//console.log (d110d);
+				} else {drawGraf = true ;} */
+			//ГО
+			
+			
+		}
+	});
+
+};
