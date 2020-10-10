e1a276afaa6dda2a891fce8df53a5abd526fa87a
Sed Ser <zxlive@yandex.ru>
2020-10-10 20:09:01 +0400

    backend

---
+++ backend/index.php
@@ -0,0 +1,64 @@
+<?php
+  //Отладка ошибок
+	ini_set('display_errors',1);
+	error_reporting(E_ALL ^E_NOTICE);
+	//Если форма авторизации отправлена...
+	if ( !empty($_POST['password']) and !empty($_POST['login']) ) {
+		//Пишем логин и пароль из формы в переменные (для удобства работы):
+		$login = $_POST['login']; 
+		$pass = $_POST['password']; 
+
+		/*
+			Формируем и отсылаем SQL запрос:
+			ВЫБРАТЬ ИЗ таблицы_users ГДЕ поле_логин = $login И поле_пароль = $password.
+    */
+    //Устанавливаем доступы к базе данных:
+		$host = 'localhost'; //имя хоста, на локальном компьютере это localhost
+		$user = 'root'; //имя пользователя, по умолчанию это root
+		$password = ''; //пароль, по умолчанию пустой
+		$db_name = 'test'; //имя базы данных
+
+	  //Соединяемся с базой данных используя наши доступы:
+    //$link = mysqli_connect($host, $user, $password, $db_name);
+    $dbc= mysqli_connect('127.0.0.1', 'goodman', 'NRywfHcXEmzenn7S') or die(mysqli_sqlstate($dbc));
+    $code_page="SET NAMES 'utf8';";
+    mysqli_query($dbc,$code_page) or die(mysqli_sqlstate($dbc));
+    $name_base="pozitron";
+    $table="USERS";
+    $query = "SELECT * FROM ".$name_base.".".$table."  WHERE login='".$login."' AND password='".$pass."';";
+    //echo $query;
+    //echo $pass;
+		$result=mysqli_query($dbc,$query) or die(mysqli_sqlstate($dbc)); //ответ базы запишем в переменную $result. 
+    $user = mysqli_fetch_assoc($result); //преобразуем ответ из БД в нормальный массив PHP
+    //echo $result;
+    //echo $query;
+    mysqli_close($dbc);
+
+		//Если база данных вернула не пустой ответ - значит пара логин-пароль правильная
+		if (!empty($user)) {
+
+			//Стартуем сессию:
+			session_start(); 
+
+			//Пишем в сессию информацию о том, что мы авторизовались:
+			$_SESSION['auth'] = true; 
+
+			//Пишем в сессию логин и id пользователя (их мы берем из переменной $user!):
+			$_SESSION['id'] = $user['id']; 
+      $_SESSION['login'] = $user['login'];
+      include './noindex.php';
+      
+      //include './noindex.php';
+		} else {
+	  //Пользователь неверно ввел логин или пароль, выполним какой-то код.
+	  //echo "<script>alert(\"Неверный пароль или логин\");</script>"; 
+      //echo 'Неверный пароль или логин'.$_SESSION['login'];
+      
+      header("Location: index.html");
+    }
+    
+	} else {
+		header("Location: index.html");
+
+	}
+?>
---
+++ backend/js/writeskvs.js
@@ -0,0 +1,122 @@
+//Создать скважину
+function writeskvs(whatdo, table, p000, skvsjson){
+	refresh = false;
+	online = false;
+	$.ajax({
+		type: "POST",
+		url: 'js/writeskvs.php',
+		data: {whatdo:whatdo, table: table, p000: p000, skvsjson:skvsjson },
+		cache: false,
+		async: false,
+		success: function(data){
+			if (Number(data)!=1) {alert('нет связи')};
+			
+		}
+	});
+
+};
+
+//Обновить скважину
+function update(whatdo, table, p000, skvsjson){
+	refresh = false;
+	online = false;
+	$.ajax({
+		type: "POST",
+		url: 'js/writeskvs.php',
+		data: {whatdo:whatdo, table: table, p000: p000, skvsjson:skvsjson },
+		cache: false,
+		async: false,
+		success: function(data){
+			if (Number(data)!=1) {alert('нет связи')};
+			//alert(data);
+			
+		}
+	});
+
+};
+
+//Удалить скважину
+function deleteskvs(whatdo, table, p000, skvsjson){
+	refresh = false;
+	online = false;
+	$.ajax({
+		type: "POST",
+		url: 'js/writeskvs.php',
+		data: {whatdo:whatdo, table: table, p000: p000, skvsjson:skvsjson },
+		cache: false,
+		async: false,
+		success: function(data){
+			if (Number(data)!=1) {alert('нет связи')};
+			//alert(data);
+			
+		}
+	});
+
+};
+
+//Чтение всех скважин
+function readskvs(whatdo, table, p000, skvsjson){
+	refresh = false;
+	online = false;
+	$.ajax({
+		type: "POST",
+		url: 'js/writeskvs.php',
+		data: {whatdo:whatdo, table: table, p000: p000, skvsjson:skvsjson },
+		cache: false,
+		dataType: 'JSON',
+		async: false,
+		success: function(response){
+			wells = {};
+			var len = response.length;
+            for(var i=0; i<len; i++){
+                var p000 = response[i].p000;
+				var skvjson = response[i].skvjson;
+				skvjson = JSON.parse(skvjson);
+				 wells[String(p000)] =  skvjson;
+				}
+		}
+	});
+
+};
+
+//Чтение всех скважинпри старте
+function readskvstart(whatdo, table, p000, skvsjson){
+	refresh = false;
+	online = false;
+	$.ajax({
+		type: "POST",
+		url: 'js/writeskvs.php',
+		data: {whatdo:whatdo, table: table, p000: p000, skvsjson:skvsjson },
+		cache: false,
+		dataType: 'JSON',
+		async: false,
+		success: function(response){
+			wells = {};
+			var len = response.length;
+            for(var i=0; i<len; i++){
+                var p000 = response[i].p000;
+				var skvjson = response[i].skvjson;
+				skvjson = JSON.parse(skvjson);
+				 wells[String(p000)] =  skvjson;
+				}
+					//Список скважин
+					$('[id^="par"]').click(function (){
+						/* console.log(this.id); */
+						skv =wells[this.id].txt;
+						wellName =wells[this.id].wellN;
+						namecmt = wellName + "kr";
+						//Сохранить в локальное хранилище
+						colPan9save(wellName,skv,namecmt,formname);
+						refresh = true;
+						read_next();
+					});
+					
+					$('#shab').click(function (){
+						/* console.log(this.id); */
+						$('#shab1').modal('show');
+					});
+		
+		}
+	});
+
+};
