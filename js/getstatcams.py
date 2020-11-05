75d0f807b517ca7ab0947a170db221e574e62ca0
Sed Ser <zxlive@yandex.ru>
2020-11-06 02:18:10 +0400

    gf

--- js/getstatcams.py
+++ js/getstatcams.py
@@ -4,13 +4,14 @@ import sys
 
 import subprocess
 
-def getstatcams(host, port, timeout, retry_interval, reset_vpn_host):
+def getstatcams():
+
    
-			subprocess.call(["curl", "-s", "-X", "POST", "https://api.telegram.org/bot885329435:AAHTWnyjFEzy_NMp4Os64Y8V_vmECgdimQA/sendMessage", "-d", "chat_id="+chat_id, "-d", "text="+message])
-			
-			print (reset_vpn_host+ ": good reboot! ")
+    # subprocess.call(["curl", "-s", "-X", "POST", "https://api.telegram.org/bot885329435:AAHTWnyjFEzy_NMp4Os64Y8V_vmECgdimQA/sendMessage", "-d", "chat_id="+chat_id, "-d", "text="+message])
+    
+    # print (reset_vpn_host+ ": good reboot! ")
 			
       
-
-getstatcams(sys.argv[1],"22",20,5, sys.argv[1])
+getstatcams
+# getstatcams(sys.argv[1],"22",20,5, sys.argv[1])
 
