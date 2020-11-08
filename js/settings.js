981e550bea95cd6588d023f73bd190d703bf36e3
Sed Ser <zxlive@yandex.ru>
2020-11-08 04:43:34 +0400

    ggg

--- js/settings.js
+++ js/settings.js
@@ -734,50 +734,7 @@ function colOK4 () {
 	// console.log(namecams);
 	getstatcams('check',namecams);
 	var frame = document.createElement("iframe");
-	frame.setAttribute("id", "camsf2");
-	frame.setAttribute("width", '100%');
-
-	frame.setAttribute("scrolling", "yes");
-	frame.setAttribute("frameborder", "0");
-	frame.setAttribute("allowfullscreen", "true");
-	frame.setAttribute("webkitAllowFullScreen", "true");
-	frame.setAttribute("mozallowfullscreen", "true");
-	frame.style.position = 'relative';
-	frame.style.width = '10%';
-	frame.style.height = '10%';
-	frame.style.align = 'center';
-
-	frame.setAttribute("auto_orient", "true");
-	frame.setAttribute("scaling", "fit");
-	frame.setAttribute("SRC", "//hydrofalll.ddns.net:5443/LiveApp/play.html?name="+streamId);
-	$('#form7').append('ОК');
-
-	$('#tabvideo').append(frame);
-
-	//exit fullmode
-	document.addEventListener('fullscreenchange', exitHandler);
-	document.addEventListener('webkitfullscreenchange', exitHandler);
-	document.addEventListener('mozfullscreenchange', exitHandler);
-	document.addEventListener('MSFullscreenChange', exitHandler);
-
-	function exitHandler() {
-		if (!document.fullscreenElement && !document.webkitIsFullScreen && !document.mozFullScreen && !document.msFullscreenElement) {
-			
-			$('#camsf2').remove();
-			// console.log('dsds');
-		}
-	}  
-	//go fullmode
-	let elem = document.querySelector("#camsf2");
-
-	if (!document.fullscreenElement) {
-		//$('#camsf2').remove();
-	elem.requestFullscreen().catch(err => {
-		alert(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
-	});
-	} else {
-	document.exitFullscreen();
-	}
+	
 /* 	PArmyDialog4.hide(); */
 
 	//$('#lupa').iziModal('close');
