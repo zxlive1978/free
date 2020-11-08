9db300ddee13e8c07e1e2f4e22afbe2777f13c4b
Sed Ser <zxlive@yandex.ru>
2020-11-09 00:23:13 +0400

    ew

--- js/cams.js
+++ js/cams.js
@@ -3,7 +3,7 @@ function getstatcams(whatdo, namecams){
         // refresh = false;
         // online = false;
         $('#form7').empty();
-        $('#form7').append('Поиск в существующих потоках....');
+        $('#form7').append('Поиск в потоках....');
         $.ajax({
             type: "POST",
             url: 'js/cams.php',
