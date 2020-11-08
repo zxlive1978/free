3a407596133b6fcc7e484ca0438f8c48075a1570
Sed Ser <zxlive@yandex.ru>
2020-11-08 04:26:35 +0400

    gf

--- js/cams.js
+++ js/cams.js
@@ -18,51 +18,51 @@ function getstatcams(whatdo, namecams){
                     if (livestatcams[keey].name==namecams && livestatcams[keey].status=='broadcasting'){
                         $('#form7').append('Найден');
                         $('#form7').append('<br>Создание iframe...');
-                        var frame = document.createElement("iframe");
-                        frame.setAttribute("id", "camsf2");
-                        frame.setAttribute("width", '100%');
+                        // var frame = document.createElement("iframe");
+                        // frame.setAttribute("id", "camsf2");
+                        // frame.setAttribute("width", '100%');
                     
-                        frame.setAttribute("scrolling", "yes");
-                        frame.setAttribute("frameborder", "0");
-                        frame.setAttribute("allowfullscreen", "true");
-                        frame.setAttribute("webkitAllowFullScreen", "true");
-                        frame.setAttribute("mozallowfullscreen", "true");
-                        frame.style.position = 'relative';
-                        frame.style.width = '0%';
-                        frame.style.height = '0%';
-                        frame.style.align = 'center';
+                        // frame.setAttribute("scrolling", "yes");
+                        // frame.setAttribute("frameborder", "0");
+                        // frame.setAttribute("allowfullscreen", "true");
+                        // frame.setAttribute("webkitAllowFullScreen", "true");
+                        // frame.setAttribute("mozallowfullscreen", "true");
+                        // frame.style.position = 'relative';
+                        // frame.style.width = '0%';
+                        // frame.style.height = '0%';
+                        // frame.style.align = 'center';
                     
-                        frame.setAttribute("auto_orient", "true");
-                        frame.setAttribute("scaling", "fit");
-                        frame.setAttribute("SRC", "//hydrofalll.ddns.net:5443/LiveApp/play.html?name="+livestatcams[keey].streamId);
-                        $('#form7').append('ОК');
+                        // frame.setAttribute("auto_orient", "true");
+                        // frame.setAttribute("scaling", "fit");
+                        // frame.setAttribute("SRC", "//hydrofalll.ddns.net:5443/LiveApp/play.html?name="+livestatcams[keey].streamId);
+                        // $('#form7').append('ОК');
                     
-                        $('#tabvideo').append(frame);
+                        // $('#tabvideo').append(frame);
                     
-                        //exit fullmode
-                        document.addEventListener('fullscreenchange', exitHandler);
-                        document.addEventListener('webkitfullscreenchange', exitHandler);
-                        document.addEventListener('mozfullscreenchange', exitHandler);
-                        document.addEventListener('MSFullscreenChange', exitHandler);
+                        // //exit fullmode
+                        // document.addEventListener('fullscreenchange', exitHandler);
+                        // document.addEventListener('webkitfullscreenchange', exitHandler);
+                        // document.addEventListener('mozfullscreenchange', exitHandler);
+                        // document.addEventListener('MSFullscreenChange', exitHandler);
                     
-                        function exitHandler() {
-                            if (!document.fullscreenElement && !document.webkitIsFullScreen && !document.mozFullScreen && !document.msFullscreenElement) {
+                        // function exitHandler() {
+                        //     if (!document.fullscreenElement && !document.webkitIsFullScreen && !document.mozFullScreen && !document.msFullscreenElement) {
                                 
-                                $('#camsf2').remove();
-                                // console.log('dsds');
-                            }
-                        }  
-                        //go fullmode
-                        let elem = document.querySelector("#camsf2");
+                        //         $('#camsf2').remove();
+                        //         // console.log('dsds');
+                        //     }
+                        // }  
+                        // //go fullmode
+                        // let elem = document.querySelector("#camsf2");
                     
-                        if (!document.fullscreenElement) {
-                            $('#camsf2').remove();
-                        elem.requestFullscreen().catch(err => {
-                            alert(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
-                        });
-                        } else {
-                        document.exitFullscreen();
-                        }
+                        // if (!document.fullscreenElement) {
+                        //     $('#camsf2').remove();
+                        // elem.requestFullscreen().catch(err => {
+                        //     alert(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
+                        // });
+                        // } else {
+                        // document.exitFullscreen();
+                        // }
                         return 0;
                     } else {
                       
