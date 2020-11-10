c1baefe53d08fbed20f79317b1523da0ecb5ac85
zxlive1978 <zxlive@yandex.ru>
2020-11-10 15:23:22 +0400

    dsa

--- js/sutki.js
+++ js/sutki.js
@@ -1889,8 +1889,17 @@ function init(){
 						// 	gfx_group.clear();
 						// });
 						gfx_group
-						.animate()
-						
+						.animate(3000).move(100, 100).during(function(pos, morph, eased, situation) {
+							// numeric values
+							gfx_group.size(morph(100, 200), morph(100, 50))
+						  
+							// unit strings
+							gfx_group.attr('cx', morph('20%', '80%'))
+						  
+							// hex color strings
+							gfx_group.fill(morph('#333', '#ff0066'))
+						  })
+
 						//.move(200, 200).reverse(true).animate().dmove(50,50).size(300,400)
 						.reverse(true)
 						.loop()
