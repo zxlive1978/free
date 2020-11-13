53719508c32f68f5f6a571bfb6bc55ed171c3cbc
zxlive1978 <zxlive@yandex.ru>
2020-11-13 09:41:27 +0400

    e

--- noindex.php
+++ noindex.php
@@ -101,7 +101,10 @@ if (!$_SESSION['auth']) {
 </nav> -->
 
 <nav class="navbar navbar-primary bg-dark py-0 navbar-fixed-top" id="bigpan">
-<a class='btn btn-outline-light' href='#'><img src='css/menu.png'  width="20" height="20"></a><button type="button" class="btn btn-sm text-light " id="skvnamelab" ><h5>АГКМ222-1</h5></button>
+<a class='btn btn-outline-light' href='#'><img src='css/menu.png'  width="20" height="20"></a>
+<!-- <button type="button" class="btn btn-sm text-light " id="skvnamelab" > -->
+  <h5>АГКМ222-1</h5>
+<!-- </button> -->
 <button type="button" class="btn btn-outline-light" onClick="exit('exit','exit','exit');">Выход</button>
 
 </nav>
