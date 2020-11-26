e9b702a9fc3657a5b765ac637004eaa4b58e7e90
zxlive1978 <zxlive@yandex.ru>
2020-11-26 16:32:49 +0400

    kkk

--- noindex.php
+++ noindex.php
@@ -106,9 +106,9 @@ if (!$_SESSION['auth']) {
 <div class="dropdown">
 <a class='btn btn-outline-light '  data-toggle="tooltip" data-placement="bottom" title="Основное меню регистрируемых данных станции ЭГТИ" onClick="$('#myModal11').modal('show');"><img src='css/menu.png'  width="20" height="20"></a>
 <button class='btn btn-outline-light dropdown-toggle'  id='dropdownMenu1' data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" data-placement="bottom" title="Инструменты" ><img src='css/tools.png'  width="20" height="20"></button>
-<ul class="dropdown-menu bg-dark" aria-labelledby="dropdownMenu1">
-    <li><img src='css/lupa.svg'  width="20" height="20"><a href="#">Действие 1</a></li>
-    <li><img src='css/menu.png'  width="20" height="20"><a href="#">Действие 2</a></li>
+<ul class="dropdown-menu dropdown-menu-left bg-dark" aria-labelledby="dropdownMenu1">
+    <li><<button class='btn btn-outline-light' ><img src='css/lock.svg'  width="40" height="40">dsadadsdadsadadsdsddasd</button></li>
+    <li><img src='css/lupa.svg'  width="40" height="40" style="margin:5px;"><a href="#">Действие 2</a></li>
     <li><img src='css/menu.png'  width="20" height="20"><a href="#">Действие 3</a></li>
     <li role="separator" class="divider"></li>
     <li><img src='css/menu.png'  width="20" height="20"><a href="#">Действие 4</a></li>
