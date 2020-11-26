083200ed00af3c33abfb5fe57d41f871639774df
zxlive1978 <zxlive@yandex.ru>
2020-11-26 12:21:18 +0400

    ds

--- js/settings.js
+++ js/settings.js
@@ -1173,6 +1173,50 @@ function colOK9 () {
 }
 
 
+//Удаление дубликатов объектов
+function removeDuplicates(arr) {
+
+    const result = [];
+    const duplicatesIndices = [];
+
+    // Перебираем каждый элемент в исходном массиве
+    arr.forEach((current, index) => {
+    
+        if (duplicatesIndices.includes(index)) return;
+    
+        result.push(current);
+    
+        // Сравниваем каждый элемент в массиве после текущего
+        for (let comparisonIndex = index + 1; comparisonIndex < arr.length; comparisonIndex++) {
+        
+            const comparison = arr[comparisonIndex];
+            const currentKeys = Object.keys(current);
+            const comparisonKeys = Object.keys(comparison);
+            
+            // Проверяем длину массивов
+            if (currentKeys.length !== comparisonKeys.length) continue;
+            
+            // Проверяем значение ключей
+            const currentKeysString = currentKeys.sort().join("").toLowerCase();
+            const comparisonKeysString = comparisonKeys.sort().join("").toLowerCase();
+            if (currentKeysString !== comparisonKeysString) continue;
+            
+            // Проверяем индексы ключей
+            let valuesEqual = true;
+            for (let i = 0; i < currentKeys.length; i++) {
+                const key = currentKeys[i];
+                if ( current[key] !== comparison[key] ) {
+                    valuesEqual = false;
+                    break;
+                }
+            }
+            if (valuesEqual) duplicatesIndices.push(comparisonIndex);
+            
+        } // Конец цикла
+    });  
+    return result;
+}
+
 //Выбор скважины
 var wellSelectBase ='';
 var wellSelectName = '';
@@ -1245,6 +1289,8 @@ function colPan10 (name_select) {
 	$("#myModal4").modal('show');	};
 	if (curtemp=='video'){
 		$('#videoN').empty();
+	
+
 		for (var keey in camswell) {
 			$('#videoN').append($('<option>',
 				{
@@ -1617,10 +1663,17 @@ function adm (){
 	$('#ul1').hide();
 	$('#taabs-1').show();
 	camscreate=true;
+	//Работающие потоки
 	workstream={};
+	
+	//Текущая скважина
+	let cam =$('#skvnamelab').text();
+	//console.log(cam);
+
 	var bigelem='<div class="container-fluid"><div class=" row text-justify">';
 	for (var keey in camswell) {
 		
+		if (cam==camswell[keey].txt){
 		namecams=camswell[keey].txt+'_'+camswell[keey].name;
 		
 		bigelem=bigelem+'<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 " >'+
@@ -1638,7 +1691,7 @@ function adm (){
 			'</div>'+
 		'</div>'+
 	'</div>';
-	}
+	}}
 	bigelem=bigelem+'</div>';
 
 	$('#taabs-1').html(bigelem);
