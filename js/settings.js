50406c0ab0ef1f4c1b4169f637cee6b224997966
zxlive1978 <zxlive@yandex.ru>
2020-12-22 09:32:11 +0400

    sa

--- js/settings.js
+++ js/settings.js
@@ -1382,7 +1382,7 @@ function colOK10() {
 			//Streaming/tracks/1901?starttime=20201214t063812z&endtime=20201214t064816z
 			//2020-12-21  01:00   2020-12-21  01:01
 			///Streaming/Channels/1701
-			let backfuture='?starttime='+strd1.split
+			let backfuture='?starttime='+strd1.replaceAll('-', '')+'t'+strt1+'00z';
 			console.log(strd1+'  '+strt1+'   '+strd2+'  '+strt2+' = '+backfuture);
 		}
 		let n = document.getElementById("videoN").options.selectedIndex;
