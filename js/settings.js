0650b7b19592cd4939dba32be6a972c11fc362ef
zxlive1978 <zxlive@yandex.ru>
2021-01-26 16:27:16 +0400

    dsa

--- js/settings.js
+++ js/settings.js
@@ -1453,7 +1453,9 @@ function colOK10() {
 		$('#skvnamelabt').text(skv);
 		//Сохранить в локальное хранилище
 		colPan9save(wellName, skv, namecmt, formname);
-		//start_time=Number(d110d[d110d.length-1]['Zaboj']-Kzoomdepth*10);
+		
+		read_now();
+		start_time=Number(d110d[d110d.length-1]['Zaboj']-Kzoomdepth*10);
 		read_now();
 	}
 
