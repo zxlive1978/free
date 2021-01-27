33d96f1b7fb9dadd9ab8b3283047bb20095a0091
zxlive1978 <zxlive@yandex.ru>
2021-01-27 13:24:15 +0400

    g

--- js/sutki.js
+++ js/sutki.js
@@ -1205,6 +1205,7 @@ function init() {
 						cur_value_x = colmn11_x0 + (d110d[d110d.length - 1][basePar[key].par]) * K_x1 - (Number(basePar[key].min)) * K_x1;
 						if (d110d[d110d.length - 1]["Zaboj"] - end_time > 0.1) {
 							cur_value_y = h1 * 100;
+						
 						}
 						else {
 							//cur_value_y = h1*100;
