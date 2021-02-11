d4169e89d6f8b780966e21075171e716947952fd
zxlive1978 <zxlive@yandex.ru>
2021-02-11 15:57:30 +0400

    sd

--- js/settings.js
+++ js/settings.js
@@ -745,7 +745,7 @@ function colOK3() {
 		refresh = false; //navigation.js
 		start_time = Number($("#depth222").val());
 		Sheet.Kzoomdepth = Number($("#zzzzooom1depth").val());
-		Kzoomdepth = Number($("#zzzzooom1depth").val());
+		Kzoomdepth = Number($("#zzzzooom1depth").val()/10);
 		read_next();
 	}
 	
