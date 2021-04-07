53cac9e020b40ad10d867652a05eb3bdc8370bd1
zxlive1978 <zxlive@yandex.ru>
2021-04-07 16:05:33 +0400

    dsa

--- noindex.php
+++ noindex.php
@@ -15,7 +15,7 @@ if (!$_SESSION['auth']) {
 <head>
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <!-- <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1"> -->
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta charset="UTF-8">
 <script>const _uz=<?php echo $_SESSION['skvjson']; ?>;</script>
 <script>const _ut=<?php echo $_SESSION['time']; ?>;</script>
