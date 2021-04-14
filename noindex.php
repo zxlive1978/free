4b19a9474da0f2a89ccb2adec0d1047baf0ae330
zxlive1978 <zxlive@yandex.ru>
2021-04-14 16:19:29 +0400

    sa

--- noindex.php
+++ noindex.php
@@ -19,8 +19,8 @@ if (!$_SESSION['auth']) {
 <script>const _uz=<?php echo $_SESSION['skvjson']; ?>;</script>
 <script>const _ut=<?php echo $_SESSION['time']; ?>;</script>
 
-<!-- <link rel="stylesheet" type="text/css" href="css/jschart5.css"> -->
-<link rel="stylesheet" href="css/iziModal.min.css">
+<link rel="stylesheet" type="text/css" href="css/jschart5.css">
+<!-- <link rel="stylesheet" href="css/iziModal.min.css"> -->
 
 <!-- Bootstrap -->
 <link href="css/bootstrap.min.css" rel="stylesheet">
