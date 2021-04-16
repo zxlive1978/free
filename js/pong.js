96a9347241cb9f612e51ba2f5a374ed8049d951a
zxlive1978 <zxlive@yandex.ru>
2021-04-16 09:11:49 +0400

    ds

--- js/pong.js
+++ js/pong.js
@@ -44,6 +44,7 @@ var wellName ="";
 var skv ="";
 var namecmt = wellName + "kr";
 var wellNamedepth ="" +"depth_all";
+var formdirdepth ="depth\\";
 var formname = "Под телефон";
 var formnamedepth = "depth\Под телефон";
 
