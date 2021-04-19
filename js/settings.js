5602ab32454c0a38452854c09d78a321dccfb22d
zxlive1978 <zxlive@yandex.ru>
2021-04-19 16:02:03 +0400

    cxz

--- js/settings.js
+++ js/settings.js
@@ -1229,11 +1229,11 @@ function colOK9() {
 		savedata($("#colitems99").val());
 		
 		
-		if (curtemp='time'){
+		if (curtemp=='time'){
 			//Сохранить в локальное хранилище
 			colPan9save(wellName, skv, namecmt, $("#colitems99").val(), formnamedepth);
 			formname = String($("#colitems99").val());}
-		if (curtemp='depth'){
+		if (curtemp=='depth'){
 			//Сохранить в локальное хранилище
 			colPan9save(wellName, skv, namecmt, formname, $("#colitems99").val());
 			formnamedepth = String($("#colitems99").val());}
