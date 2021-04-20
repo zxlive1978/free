ef36f43112b7f81a629fcfddf10edc9538478da8
zxlive1978 <zxlive@yandex.ru>
2021-04-20 12:54:51 +0400

    dsa

--- js/settings.js
+++ js/settings.js
@@ -1204,7 +1204,7 @@ function colPan9(name_select) {
 	}
 	//`Выбор файла для перезаписи
 	$("#colitems9").change(function () {
-		$("#colitems99").val($("#colitems9")[0].selectkeey);
+		$("#colitems99").val($("#colitems9")[0].selectedIndex);
 	});
 	//Длина объекта
 	/* alert(filesss.length); */
