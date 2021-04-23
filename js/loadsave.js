ba33ae60b99580ff624cd057a60944814664f7eb
zxlive1978 <zxlive@yandex.ru>
2021-04-23 16:00:47 +0400

    dsa

--- js/loadsave.js
+++ js/loadsave.js
@@ -111,6 +111,7 @@ function loadddata(name) {
 				basePar = plan[2];
 				txtPar = plan[3];
 				txtOknOPar = plan[4];
+				if (plan[5]==null){ var geoOknOPar={};}
 				geoOknOPar = plan[5];
 				read_now();
 				//Перерисовка
