168d2ba54acf6416b746df0c332ad325d743bfb1
zxlive1978 <zxlive@yandex.ru>
2021-04-26 16:28:41 +0400

    dsa

--- js/sutki.js
+++ js/sutki.js
@@ -1323,7 +1323,7 @@ for (var key in geoOknOPar) {
 
 		var img= draw.image('css/1.bmp')
 		var pattern = draw.pattern(20, 20, function(add) {
-			add.img
+			add.img();
 		  })
 		var rec=draw.rect(100, 100)
 		.radius(10).fill(pattern)
