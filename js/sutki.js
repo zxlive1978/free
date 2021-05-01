e5341a8910b74bb625f255a5465c47d938a15ec3
Sed Ser <zxlive@yandex.ru>
2021-05-02 01:42:51 +0400

    fd

--- js/sutki.js
+++ js/sutki.js
@@ -1694,7 +1694,7 @@ for (var key in geoOknOPar) {
 				var addon=0;
 				for (var keys in d110l) {
 					if ((geoOknOPar[key].par=='Litol' && d110l[keys].type =='1') || (geoOknOPar[key].par=='Shlam' && d110l[keys].type =='0')){
-						if (kurnumb<Number(d110l[keys].numb) ){
+						if ((kurnumb<Number(d110l[keys].numb)) &&  (oldgeo.top == d110l[keys].top) && (oldgeo.bot== d110l[keys].bot)){
 							addon=oldgeo.proc * K_x1;
 						} else {
 							addon=0;
