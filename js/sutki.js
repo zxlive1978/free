72067cdeb6afaf558cebc9d3a962885d3a2b5ce9
Sed Ser <zxlive@yandex.ru>
2021-05-02 18:33:56 +0400

    nn

--- js/sutki.js
+++ js/sutki.js
@@ -1339,9 +1339,9 @@ for (var key in geoOknOPar) {
 
 		//тестирование отрисовки текстуры
 		// var img= draw.image('css/1.bmp')
-		var txt='4';
+		var txt='1';
 		var pattern = draw.pattern(16, 16, function(add) {
-			add.svg(ref_rocks.rock+txt.img);
+			add.svg(ref_rocks['rock'+txt].img);
 		  })
 
 		// var rec=draw.rect(100, 100)
