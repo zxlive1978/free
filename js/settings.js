c0194ed348c30acc97b70bbf554514c26dd772b2
Sed Ser <zxlive@yandex.ru>
2021-05-02 19:46:02 +0400

    sd

--- js/settings.js
+++ js/settings.js
@@ -425,9 +425,9 @@ var ref_rocks = {
 		img:'<?xml version="1.0" encoding="UTF-8" standalone="no"?> <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"> <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16px" height="16px" viewBox="0 0 16 16" enable-background="new 0 0 16 16" xml:space="preserve"> <image id="image0" width="16" height="16" x="0" y="0" xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAgMAAABinRfyAAAABGdBTUEAALGPC/xhBQAAACBjSFJN AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAACVBMVEX//////wD+/v4/kYjv AAAAAWJLR0QCZgt8ZAAAAAlwSFlzAAAOxAAADsQBlSsOGwAAAAd0SU1FB+UFAhA5JMYVrXEAAAAa SURBVAjXY2AAAdHQAAEGNBAgIBqKLkZYHQCGdAMpBKKpmAAAACV0RVh0ZGF0ZTpjcmVhdGUAMjAy MS0wNS0wMlQxNjo1NzozNiswMzowMPMVvkwAAAAldEVYdGRhdGU6bW9kaWZ5ADIwMjEtMDUtMDJU MTY6NTc6MzYrMDM6MDCCSAbwAAAAAElFTkSuQmCC" /> </svg> ',
 		txt:'Алеврит'
 	},
-	rock:{
-		img:'',
-		txt:''
+	rock212:{
+		img:'<?xml version="1.0" encoding="UTF-8" standalone="no"?> <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"> <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16px" height="16px" viewBox="0 0 16 16" enable-background="new 0 0 16 16" xml:space="preserve"> <image id="image0" width="16" height="16" x="0" y="0" xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAgMAAABinRfyAAAABGdBTUEAALGPC/xhBQAAACBjSFJN AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAACVBMVEX//////wD+/v4/kYjv AAAAAWJLR0QCZgt8ZAAAAAlwSFlzAAAOxAAADsQBlSsOGwAAAAd0SU1FB+UFAhA5JMYVrXEAAAAd SURBVAjXY2AAgVAgYEADAQKiGGLY1ImGBgggeAC/DgQ9mf/66AAAACV0RVh0ZGF0ZTpjcmVhdGUA MjAyMS0wNS0wMlQxNjo1NzozNiswMzowMPMVvkwAAAAldEVYdGRhdGU6bW9kaWZ5ADIwMjEtMDUt MDJUMTY6NTc6MzYrMDM6MDCCSAbwAAAAAElFTkSuQmCC" /> </svg> ',
+		txt:'Алевропесчаник'
 	},
 	rock:{
 		img:'',
