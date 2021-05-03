d1ddf061eb68146c60ef2ada37f4c82d864accc0
Sed Ser <zxlive@yandex.ru>
2021-05-03 16:09:30 +0400

    fd

--- js/read_litholog.php
+++ js/read_litholog.php
@@ -32,7 +32,7 @@
 	
 	//read all
 	if ( $whatdo == 'read'){
-		$query="SELECT * FROM ".$name_base.".".$table." WHERE (".$x_id_top.">=".(int)$start_time." AND ".$x_id_top."<=".(int)$end_time.") OR (".$x_id_bot.">=".(int)$start_time." AND ".$x_id_bot."<=".(int)$end_time.") OR (".$x_id_top."<=".(int)$start_time." AND ".$x_id_bot.">=".(int)$end_time.") ORDER BY top;";
+		$query="SELECT * FROM ".$name_base.".".$table." WHERE (".$x_id_top.">=".(int)$start_time." AND ".$x_id_top."<=".(int)$end_time.") OR (".$x_id_bot.">=".(int)$start_time." AND ".$x_id_bot."<=".(int)$end_time.") OR (".$x_id_top."<=".(int)$start_time." AND ".$x_id_bot.">=".(int)$end_time.") ORDER BY top, numb;";
 		$result=mysqli_query($dbc,$query) or die(mysqli_sqlstate($dbc));
 		$comment = array();
 		while($row=mysqli_fetch_array($result,MYSQLI_ASSOC)){
