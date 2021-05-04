20314f46a1bc32b9867ccc88c4eccc23d7bf261d
Sed Ser <zxlive@yandex.ru>
2021-05-04 17:08:52 +0400

    g

--- js/sutki.js
+++ js/sutki.js
@@ -2221,7 +2221,7 @@ if (curtemp=='depth'){
 			if (strix !='col0'){
 				// var line = draw.line(w1 * time_w, beg_plats, w1 * 100, beg_plats);
 				// line.stroke({ width: Sheet.width_line_p, color: Sheet.dashcol1, dasharray: Sheet.dasharray });
-				var line = draw.line(strix.poz.x*w1, beg_plats, strix.poz.x*w1+strix.size.w*w1, beg_plats);
+				var line = draw.line(Columns[strix].poz.x*w1, beg_plats, Columns[strix].poz.x*w1+Columns[strix].size.w*w1, beg_plats);
 				line.stroke({ width: Sheet.width_line_p, color: Sheet.dashcol1, dasharray: Sheet.dasharray });
 			}
 		}
