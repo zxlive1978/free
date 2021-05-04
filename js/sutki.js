8d1dfad9bd1009d863ade056f1f50ffec095743d
Sed Ser <zxlive@yandex.ru>
2021-05-04 17:27:13 +0400

    lkj

--- js/sutki.js
+++ js/sutki.js
@@ -2217,14 +2217,17 @@ if (curtemp=='depth'){
 
 		//Маленькие насечки пунктир на все графики
 		//Линия пунктирная
-		for (var strix in Columns){
-			if (strix !='col0'){
-				// var line = draw.line(w1 * time_w, beg_plats, w1 * 100, beg_plats);
-				// line.stroke({ width: Sheet.width_line_p, color: Sheet.dashcol1, dasharray: Sheet.dasharray });
-				var line = draw.line(Columns[strix].poz.x*w1, beg_plats, Columns[strix].poz.x*w1+Columns[strix].size.w*w1, beg_plats);
-				line.stroke({ width: Sheet.width_line_p, color: Sheet.dashcol1, dasharray: Sheet.dasharray });
-			}
-		}
+		// for (var strix in Columns){
+		// 	if (strix !='col0'){
+		// 		// var line = draw.line(w1 * time_w, beg_plats, w1 * 100, beg_plats);
+		// 		// line.stroke({ width: Sheet.width_line_p, color: Sheet.dashcol1, dasharray: Sheet.dasharray });
+		// 		var line = draw.line(Columns[strix].poz.x*w1, beg_plats, Columns[strix].poz.x*w1+Columns[strix].size.w*w1, beg_plats);
+		// 		line.stroke({ width: Sheet.width_line_p, color: Sheet.dashcol1, dasharray: Sheet.dasharray });
+		// 	}
+		// }
+		var line = draw.line(w1 * time_w, beg_plats, w1 * 100, beg_plats);
+		line.stroke({ width: Sheet.width_line_p, color: Sheet.dashcol1, dasharray: Sheet.dasharray });
+				
 		//Цифровые значения шкалы
 		if (big_teth) {
 			
