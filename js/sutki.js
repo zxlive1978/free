57dbadb253b75450808103cf218e956600f5903c
Sed Ser <zxlive@yandex.ru>
2021-05-09 14:00:27 +0400

    ав

--- js/sutki.js
+++ js/sutki.js
@@ -3040,7 +3040,7 @@ if (curtemp=='depth'){
 
 						// 	if (disp_val1 > 0) { cur_val = String(d110d[disp_val1][txtPar[keey].par]) };
 						// 	if (Number(cur_val) <= -2147480) { cur_val = "NaN"; }
-							var text_value = draw.text(ref_rocks["rock"+String(findRocks[keey].code)].txt + " " + String(findRocks[keey].pro*100) + " (%)")
+							var text_value = draw.text(ref_rocks["rock"+String(findRocks[keey].code)].txt + " " + String((findRocks[keey].pro*100).toFixed(1)) + " (%)")
 								.font({ family: Sheet.fnt, size: text_size_value })
 								// .move(X_cur_mouse_click, Y_cur_mouse_click -h1*Sheet.height_value + h1*all_step)
 								.move(X_cur_mouse_click, Y_cur_mouse_click - h1 * Sheet.height_value + h1 * all_step)
