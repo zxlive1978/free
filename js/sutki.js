b251b74bfc78680c21afdc8c8c288d8798d40db3
Sed Ser <zxlive@yandex.ru>
2021-05-09 14:09:58 +0400

    hg

--- js/sutki.js
+++ js/sutki.js
@@ -3037,7 +3037,8 @@ if (curtemp=='depth'){
 						.move(X_cur_mouse_click, Y_cur_mouse_click - h1 * Sheet.height_value + h1 * all_step)
 						.cx(X_cur_mouse_click);
 						gfx_group.add(recrock);
-						let real_proc = (findRocks[keey].proс*100).toFixed(1);
+						let proc =findRocks[keey].proс*100;
+						let real_proc = proc.toFixed(1);
 						console.log(real_proc);
 
 						// 	if (disp_val1 > 0) { cur_val = String(d110d[disp_val1][txtPar[keey].par]) };
