1cb4bb32590ad52b5fe2231f9e8444a5f520991a
Sed Ser <zxlive@yandex.ru>
2021-05-23 00:42:09 +0400

    jjj

--- js/settings.js
+++ js/settings.js
@@ -2453,7 +2453,7 @@ function adm5() {
 	// 		$('#taabs-3').html(bigelem);
 	// 	}
 	// });
-	bigelem= bigelem+'<iframe src="https://hydrofalll.ddns.net/freedepth/report/542pdf.pdf" style="width:100%;height:700px;"></iframe>'+ '</div>';
+	bigelem= bigelem+'<iframe src="https://hydrofalll.ddns.net/freedepth/report/542.html" style="width:100%;height:700px;"></iframe>'+ '</div>';
 	$('#taabs-3').html(bigelem);
 // import pdfkit
 
