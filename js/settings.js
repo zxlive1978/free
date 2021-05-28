fabe9fae915fe99d107837db3c285ee1e337b354
zxlive1978 <zxlive@yandex.ru>
2021-05-28 15:27:49 +0400

    fds

--- js/settings.js
+++ js/settings.js
@@ -1800,7 +1800,7 @@ function colPan10(name_select) {
 			//`Выбор файла для перезаписи
 		$("#colitems17").change(function () {
 			$("#colitems18").val($('input[name="colitems17"]:checked').closest('label').text());
-			cur_svodka=$('input[name="colitems17"]:checked').closest('label').text();
+			cur_svodka=$('select[@name="colitems17"] option:selected').val();;
 			console.log(cur_svodka);
 		});
 		$("#myModal21").modal('show');
