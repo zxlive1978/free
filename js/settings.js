46ae15aa0362b7f3d284b406cb61fcc34f857bc7
zxlive1978 <zxlive@yandex.ru>
2021-05-28 11:02:55 +0400

    sa

--- js/settings.js
+++ js/settings.js
@@ -2463,7 +2463,7 @@ function adm5() {
 	// 		$('#taabs-3').html(bigelem);
 	// 	}
 	// });
-	bigelem= bigelem+'<iframe src="https://hydrofalll.ddns.net'+files_svodka[0].slice(5)+'" style="width:100%;height:100%;"></iframe>'+ '</div>';
+	bigelem= bigelem+'<iframe src="https://hydrofalll.ddns.net'+files_svodka[10].slice(5)+'" style="width:100%;height:100%;"></iframe>'+ '</div>';
 	$('#taabs-3').html(bigelem);
 // import pdfkit
 
