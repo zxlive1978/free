afffe9d049ad3e7f445d7684979725764b3cce30
zxlive@1978 <zxlive1978@yandex.ru>
2021-06-03 16:26:52 +0400

    x

--- js/settings.js
+++ js/settings.js
@@ -1756,7 +1756,7 @@ function colPan10(name_select) {
 				}));
 		}
 		//Восстановление выбора
-		$('#svodkaoN').val(wellSelectName);
+		$('#svodkaoN').val(wellName);
 
 
 		if ((Object.keys(files_svodka).length > 0)) {
@@ -1767,7 +1767,6 @@ function colPan10(name_select) {
 		$('#svodkaoN').on('change', function () {
 			wellSelectBase = $("#svodkaoN").prop('value');
 			wellSelectName = $("#svodkaoN option:selected").text();
-			$('#svodkaoN').val(wellSelectName);
 			$('#colitems17').empty();
 			for (var cur in files_svodka) {
 				// console.log(cur);
