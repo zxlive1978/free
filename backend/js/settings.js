14cc1bf80a171a3258f6f93a46d7448afc446642
Sedyshev Serge <zxlive@yandex.ru>
2021-07-13 16:28:43 +0400

    вы

--- backend/js/settings.js
+++ backend/js/settings.js
@@ -1675,11 +1675,19 @@ function adm (){
   	+'<input type="file" class="custom-file-input" id="files"  lang="ru">'
   	+'<label class="custom-file-label" for="customFileLang">Выберите файл</label>'
 	+'</div></div></div>'
-	+'<input class="btn btn-success" type="submit" value="Загрузить">'
+	
 	// +'<tr><td><div id="message">Выберите файл:</div></td><td><input type="file" id="files" name="files[]" /></td><td></td></t>'
     // +'</table>'
     // +'<input type="submit" value="Загрузить &gt;&gt;" /></form>'
+	+'<div class="form-row">'
+    +'<div class="col-md-4 mb-3">'
+	+'<input class="btn btn-success" type="submit" value="Загрузить">'
 	
+	+'<div class="progress">'
+  	+'<div class="progress-bar progress-bar-striped bg-success-sm" role="progressbar" style="width: 0%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>'
+	+'</div></div>'
+	+'</div></div>'
+
 	+'<div id="cnuploader_progressbar"></div>'
 	+'<div id="cnuploader_progresscomplete"></div>'
 	+'<div id="c"><br><br></div>'
