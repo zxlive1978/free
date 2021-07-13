a4d8d026f64ec1a5f87a72840963fddb41504298
Sedyshev Serge <zxlive@yandex.ru>
2021-07-13 13:39:20 +0400

    ds

--- backend/js/settings.js
+++ backend/js/settings.js
@@ -1730,7 +1730,7 @@ function ShowForm() {
         uploadscript: 'js/upload.php',
 
         // URL, куда перенаправить пользователя при успешной загрузке
-        redirect_success: '../',
+        // redirect_success: '../',
 
         // URL, куда отправить пользователя при ошибке загрузки
         redirect_abort: './abort.php?hash=<?php print $hash;?>',
