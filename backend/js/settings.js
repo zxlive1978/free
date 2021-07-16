1a2519dbebf2806ed974499aadd143c558a80e45
Sedyshev Serge <zxlive@yandex.ru>
2021-07-16 13:37:09 +0400

    dsa

--- backend/js/settings.js
+++ backend/js/settings.js
@@ -1684,8 +1684,8 @@ function adm (){
 	// +'<label for="hcolcolor">Загрузка архива *.zip( *.dep + *.lst)</label>'
 	
 	+'<div class="form-row ">'
-    +'<div class="col-md-3 mb-3">'
-  	+'<input type="file" class="custom-file-input" id="files"  lang="ru">'
+    +'<div class="col-md-3 mb-3 border">'
+  	+'<input type="file" class="custom-file-input p-4" id="files"  lang="ru"></input>'
   	+'<label class="custom-file-label" for="customFileLang">Загрузка архива *.zip( *.dep + *.lst)</label>'
 	  +'<div class="progress">'
   	+'<div id="uploadprogress" class="progress-bar progress-bar-striped bg-success progress-bar-animated" role="progressbar" style="width:0%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>'
@@ -1709,11 +1709,12 @@ function adm (){
 	// +'</div></div>'
 	
 	+'<div class="form-row ">'
-    +'<div class="col-md-3 mb-3 p-4 border border-secondary ">'
+    +'<div class="col-md-3 mb-3 p-4 border ">'
 	+'<div class="form-group ">'
 	+'<label for="files_dirka">Список архивов на сервере</label>'
 	
     +'<select class="form-control" id="files_dirka" size="5"></select><br>' 
+	+'<input class="btn btn-danger" name="deletedirka" value="Удалить">'
 	+'<label for="hcolcolor"class="control-label">Начало:</label>'
     +'<input type="date" class="form-control  " id="startdirka" name="startdirka" ><input type="time"  class="form-control" id="startdirka2" name="startdirka2" class="form-control  " value="01:00:00" step="1" min="00:00" max="24:00" required >'
     +'<label for="hcolcolor"class="control-label">Конец:</label>'
