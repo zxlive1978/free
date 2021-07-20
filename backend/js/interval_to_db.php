669e6e16d2ca2821473c658dbea608226a4a2bde
zxlive1978 <zxlive@yandex.ru>
2021-07-20 11:48:59 +0400

    xz

--- backend/js/interval_to_db.php
+++ backend/js/interval_to_db.php
@@ -2,12 +2,14 @@
 	/* Отладка ошибок */
 	ini_set('display_errors',1);
 	error_reporting(E_ALL ^E_NOTICE);
-	
-
-	$registration = $_POST['registration'];
-	$host= $_POST['host'];
+	// python dtcis_read_random.py Time_024.dep s401 14:28:00-07/07/2021 15:40:00-07/07/2021
+	$arch = $_POST['arch'];
+	$table= $_POST['table'];
+	$start_int = $_POST['start_int'];
+	$stop_int= $_POST['stop_int'];
+	$whatdo= $_POST['whatdo'];
 		
-	$cmd = '/usr/bin/python /var/www/html/mon/poz/ubireboot.py '.$host;
+	$cmd = '/usr/bin/python /var/www/html/mon/poz/dtcis_read_random.py '.$host;
 	$output = shell_exec($cmd);
 	echo $output;
 ?>
