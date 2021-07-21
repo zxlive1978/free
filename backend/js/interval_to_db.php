1cb18295c59578f3e58e4dae83e8f18cf5af27e3
zxlive1978 <zxlive@yandex.ru>
2021-07-21 15:36:46 +0400

    dsa

--- backend/js/interval_to_db.php
+++ backend/js/interval_to_db.php
@@ -12,12 +12,14 @@
 	$ret_code=123;
 	$output="";
 		
-	$cmd = "/usr/bin/python /var/www/html/mon/poz/readrandom/dtcis_read_random.py ".$arch." ".$table." ".$start_int." ".$stop_int." ".$whatdo;
+	$cmd = "/usr/bin/python /var/www/html/mon/poz/readrandom/dtcis_read_random.py ".$arch." ".$table." ".$start_int." ".$stop_int." ".$whatdo." > /dev/null 2>/dev/null &";
 	// exec('python blibble.py', $output, $ret_code);
 	// $output=shell_exec($cmd);
-	exec($cmd, $output, $ret_code);
-	$out = array_values($output);
+	// exec($cmd, $output, $ret_code);
+	$output = shell_exec($cmd);
+	// $out = array_values($output);
 	// echo ($arch.' '.$table.' '.$start_int.' '.$stop_int.' '.$whatdo);
-	echo json_encode($out);
+	// echo json_encode($out);
+	echo json_encode($output);
 	// echo ini_get('max_execution_time');
 ?>
