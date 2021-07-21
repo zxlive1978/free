d0d24a87b8283dda309147e6bca2740a27c2282e
zxlive1978 <zxlive@yandex.ru>
2021-07-21 09:37:37 +0400

    xz

--- backend/js/writeskvs.js
+++ backend/js/writeskvs.js
@@ -355,8 +355,11 @@ function add_interval_to_db(whatdo, arch, table, start_int, stop_int) {
 			async: false,
 			data: {whatdo:whatdo, arch:arch, table:table, start_int:start_int, stop_int:stop_int},
 			success: function(data){
-				
-					alert(data);
+				let mess='';
+				for (var kesy in data) {
+					mess= mess+data[kesy];
+				}
+					alert(mess);
 				
 			}
 		});
